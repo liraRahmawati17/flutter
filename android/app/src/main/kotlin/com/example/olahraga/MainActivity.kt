ecd1f502cd4214752959f643061b7abc0751b47b
liraRahmawati17 <lira_180@smkassalaambandung.sch.id>
2022-03-20 21:08:26 -0700

    flutter

---
+++ android/app/src/main/kotlin/com/example/olahraga/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.olahraga
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'dart:convert';
+
+import 'package:olahraga/sample_json.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:olahraga/profile.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ProfilePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future _loadSampleJson() async {
+    String jsonString = await rootBundle.loadString("assets/sample.json");
+    final jsonData = json.decode(jsonString);
+    Sample sample = Sample.fromJson(jsonData);
+    setState(() {
+      _jsonContent = sample.toString();
+    });
+  }
+
+  late String _jsonContent = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Read Json'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      _loadSampleJson();
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 150,
+                      color: Colors.grey,
+                      alignment: Alignment.center,
+                      child: const Text("Read JSON File"),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 12.0),
+                child: Text(
+                  _jsonContent,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
